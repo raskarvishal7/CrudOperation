0e114b04955da247b6a51ab6cb2908e8d2dd7e33
Vishal Raskar <raskarvishal7@gmail.com>
2019-10-03 15:56:08 +0530

    Update Changes

--- src/app/shared/employee.model.ts
+++ src/app/shared/employee.model.ts
@@ -1,7 +1,7 @@
 export class Employee {
-    EmployeeID?: number[];
-    FullName?: string;
-    EMPCode?: string;
-    Mobile?: string;
-    Position?: string;
+    EmployeeID: number;
+    FullName: string;
+    EMPCode: string;
+    Mobile: string;
+    Position: string;
 };
