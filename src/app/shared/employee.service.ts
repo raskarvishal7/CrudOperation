485f394af8d130e8837d113ce6d9b2aa4ab12f82
Vishal Raskar <raskarvishal7@gmail.com>
2019-10-03 14:49:46 +0530

    My First Commit

---
+++ src/app/shared/employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class EmployeeService {
+  formData = Employee;
+
+  readonly rootURL = "http://localhost:51248/api";
+
+  constructor(private http : HttpClient) { }
+
+  postEmployee(formData : Employee){
+    return this.http.post(this.rootURL + '/Employee', formData);
+  }
+}
